a397a846200c823d3f5a92ccacc277d3f34a779b
unknown <puspadewin@gmail.com>
2025-01-18 17:46:46 +0700

    push assertions

--- tests/assertions.spec.js
+++ tests/assertions.spec.js
@@ -2,11 +2,53 @@
 const { test, expect } = require('@playwright/test');
 const { default: newActions} = require('../tests/actions/newActions');
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('locator dan assertions', async ({page}) => {
+    await page.goto('https://www.saucedemo.com/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+    const inputUsername = page.locator('#user-name');
+    await inputUsername.fill('standard_user');
+    await expect (inputUsername).toHaveValue('standard_user');
+
+    const inputPassword = page.locator('#password');
+    await inputPassword.fill('secret_sauce');
+    await expect (inputPassword).toHaveValue('secret_sauce');
+
+    const buttonLogin = page.locator('#login-button');
+    await buttonLogin.click();
+
+    const addtocartback = page.locator('#add-to-cart-sauce-labs-backpack');
+    await addtocartback.click();
+
+    const addtocartJacket = page.locator('#add-to-cart-sauce-labs-fleece-jacket');
+    await addtocartJacket.click();
+
+    const listcart = page.locator('#shopping_cart_container > a');
+    await listcart.click();
+
+    const checkout = page.locator('#checkout');
+    await checkout.click();
+
+    const inputFName = page.locator('#first-name');
+    await inputFName.fill('Puspa');
+    await expect (inputFName).toHaveValue('Puspa');
+
+    const inputLName = page.locator('#last-name');
+    await inputLName.fill('Puspa123');
+    await expect (inputLName).toHaveValue('Puspa123');
+
+    const inputPostalCode = page.locator('#postal-code');
+    await inputPostalCode.fill('12345');
+    await expect (inputPostalCode).toHaveValue('12345');
+
+    const Continue = page.locator('#continue');
+    await Continue.click();
+
+    const Finish = page.locator('#finish');
+    await Finish.click();
+});
+
+test('PMO weekend', async ({page}) => {
+
 });
 
 
